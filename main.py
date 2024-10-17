03f8e83390a305be7ecc97a138a4ca7a36f5655b
agung <aprabowo03@gmail.com>
2024-10-17 22:57:53 +0700

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index ():
-    return 'Welcome to the flask world version2 '
+    return 'Welcome to the flask world version2 update '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
