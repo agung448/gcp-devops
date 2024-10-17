4e031b6304ce66eadf5f093df86657b870065ad1
agung <aprabowo03@gmail.com>
2024-10-17 22:53:07 +0700

    first-update

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from Flask import Flask 
+app = Flask(__name__)
+
+@app.route('/') 
+def index ():
+    return 'Welcome to the flask world version2 '
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
